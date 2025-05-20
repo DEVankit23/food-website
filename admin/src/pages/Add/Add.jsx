d33f983d6fd0aa1491ed3f7b1bd3ecce8fcbe2c3
DEVankit23 <103306309+DEVankit23@users.noreply.github.com>
2025-05-20 12:06:41 +0530

    Second Commit

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -75,7 +75,7 @@ const Add = ({url}) => {
               <option value="Deserts">Deserts</option>
               <option value="Sandwich">Sandwich</option>
               <option value="Cake">Cake</option>
-              <option value="Pure veg">Pure Veg</option>
+              <option value="Pure Veg">Pure Veg</option>
               <option value="Pasta">Pasta</option>
               <option value="Noodles">Noodles</option>
             </select>
